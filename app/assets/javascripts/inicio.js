f95a6ebacf34963ea307afb9f2e95a2078b53ea6
Liber Dovat <chrono.liber@gmail.com>
2015-02-25 12:19:15 -0200

    probando animaciones con javascript

--- app/assets/javascripts/inicio.js
+++ app/assets/javascripts/inicio.js
@@ -1,2 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+function toggle_visibility(id) {
+    $(id).stop().animate({height:'toggle'}, 500);
+}
+
+// $('#tab').click(function() {
+//    console.log("$('#tab').click");
+//    toggle_visibility('#panel');
+// });
+
+$('#tab').on('click', function() {
+   console.log("$('#tab').click");
+   toggle_visibility('#panel');
+});
