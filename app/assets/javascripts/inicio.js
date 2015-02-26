8287d5e019c2a5091525c293a6655b3de0808c51
Liber Dovat <liber.dovat@gmail.com>
2015-02-26 01:17:33 -0200

    probando colores

--- app/assets/javascripts/inicio.js
+++ app/assets/javascripts/inicio.js
@@ -1,18 +1,3 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-function toggle_visibility(id) {
-   alert("¡Mensaje! 2 " + id);
-   $(id).stop().animate({height:'toggle'}, 500);
-}
-
-// $('#tab').click(function() {
-//    console.log("$('#tab').click");
-//    toggle_visibility('#panel');
-// });
-
-$('#tab').click(function() {
-   console.log("$('#tab').click");
-   alert("¡Mensaje!");
-   toggle_visibility('#panel');
-});
