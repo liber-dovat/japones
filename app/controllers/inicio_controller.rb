3bfa784aeb9f9198cab577e02c0e59ef352bd25b
Liber Dovat <chrono.liber@gmail.com>
2015-04-10 16:39:23 -0300

    editado el seeds, agregando más casos

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -1,6 +1,7 @@
 class InicioController < ApplicationController
   def index
 #     @kanji = Kanji.find_by(romaji: 'yon')
+#     http://stackoverflow.com/questions/2752231/random-record-in-activerecord
     offset = rand(Kanji.count)
     @kanji = Kanji.offset(offset).first
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,116 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# cambiar hiragana y romaji a lecturas de tipo string, ej:
+# 四, yon, shi
+
+# http://www.jlptstudy.net/N5/N5_kanji.html
+
+# N5
+
 Kanji.create(kanji: "一", hiragana: "いち", romaji: "ichi", nivel: 5, definicion: "uno", ejemplo1: "", ejemplo2: "")
 Kanji.create(kanji: "二", hiragana: "に", romaji: "ni", nivel: 5, definicion: "dos", ejemplo1: "", ejemplo2: "")
 Kanji.create(kanji: "三", hiragana: "さん", romaji: "san", nivel: 5, definicion: "tres", ejemplo1: "", ejemplo2: "")
 Kanji.create(kanji: "四", hiragana: "よん", romaji: "yon", nivel: 5, definicion: "cuatro", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "五", hiragana: "ご", romaji: "go", nivel: 5, definicion: "cinco", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "六", hiragana: "ろく", romaji: "roku", nivel: 5, definicion: "seis", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "七", hiragana: "なな", romaji: "nana", nivel: 5, definicion: "siete", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "八", hiragana: "はち", romaji: "hachi", nivel: 5, definicion: "ocho", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "九", hiragana: "きゅう", romaji: "kyuu", nivel: 5, definicion: "nueve", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "十", hiragana: "じゅう", romaji: "juu", nivel: 5, definicion: "diez", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "百", hiragana: "ひゃく", romaji: "hyaku", nivel: 5, definicion: "cien", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "千", hiragana: "せん", romaji: "sen", nivel: 5, definicion: "mil", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "万", hiragana: "まん", romaji: "man", nivel: 5, definicion: "diez mil", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "月", hiragana: "つき", romaji: "tsuki", nivel: 5, definicion: "luna", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "火", hiragana: "か", romaji: "ka", nivel: 5, definicion: "fuego", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "水", hiragana: "みず", romaji: "mizu", nivel: 5, definicion: "agua", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "木", hiragana: "き", romaji: "ki", nivel: 5, definicion: "árbol", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "金", hiragana: "きん", romaji: "kin", nivel: 5, definicion: "oro, dinero", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "土", hiragana: "ど", romaji: "do", nivel: 5, definicion: "tierra", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "日", hiragana: "ひ", romaji: "hi", nivel: 5, definicion: "sol, día", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "人", hiragana: "ひと", romaji: "hito", nivel: 5, definicion: "persona", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "上", hiragana: "うえ", romaji: "ue", nivel: 5, definicion: "arriba", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "下", hiragana: "した", romaji: "ue", nivel: 5, definicion: "abajo", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "中", hiragana: "なか", romaji: "naka", nivel: 5, definicion: "centro, dentro, mientras, durante", ejemplo1: "", ejemplo2: "")
 
 Kanji.create(kanji: "自動販売機", hiragana: "じどうはんばいき", romaji: "jidouhanbaiki",  nivel: 3, definicion: "máquina expendedora", ejemplo1: "", ejemplo2: "")
+
+# 今
+# 休
+# 会
+# 何
+# 先
+# 入
+# 円
+# 出
+# 分
+# 前
+# 北
+# 午
+# 半
+# 南
+# 友
+# 口
+# 古
+# 右
+# 名
+# 国
+# 外
+# 多
+# 大
+# 天
+# 女
+# 子
+# 学
+# 安
+# 小
+# 少
+# 山
+# 川
+# 左
+# 年
+# 店
+# 後
+# 手
+# 新
+# 時
+# 書
+# 本
+# 来
+# 東
+# 校
+# 母
+# 毎
+# 気
+# 父
+# 生
+# 男
+# 白
+# 目
+# 社
+# 空
+# 立
+# 耳
+# 聞
+# 花
+# 行
+# 西
+# 見
+# 言
+# 話
+# 語
+# 読
+# 買
+# 足
+# 車
+# 週
+# 道
+# 長
+# 間
+# 雨
+# 電
+# 食
+# 飲
+# 駅
+# 高
+# 魚
