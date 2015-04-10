9d8d2084699b4dcaf0baa14aa0a1a2fa3ab6cbf5
Liber Dovat <chrono.liber@gmail.com>
2015-04-10 12:50:34 -0300

    agregado el modelo Kanji, y creado el archivo seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150410152924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "kanjis", force: :cascade do |t|
+    t.text     "kanji"
+    t.text     "hiragana"
+    t.text     "romaji"
+    t.text     "nivel"
+    t.string   "definicion"
+    t.string   "ejemplo1"
+    t.string   "ejemplo2"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Kanji.create(kanji: "一", hiragana: "いち", romaji: "ichi", nivel: 1, definicion: "uno", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "二", hiragana: "に",   romaji: "ni",   nivel: 1, definicion: "dos", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "三", hiragana: "さん", romaji: "san",  nivel: 1, definicion: "tres", ejemplo1: "", ejemplo2: "")
+Kanji.create(kanji: "四", hiragana: "よん", romaji: "yon",  nivel: 1, definicion: "cuatro", ejemplo1: "", ejemplo2: "")
+
