d68936c832ca34e8836e41041288ec0657da4737
Liber Dovat <chrono.liber@gmail.com>
2015-04-13 17:26:40 -0300

    agregando el comportamiento de siguiente

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -1,8 +1,17 @@
 class InicioController < ApplicationController
+  
   def index
+    offset = rand(Kanji.count)
+    @kanji = Kanji.offset(offset).first
+  end
+  
+  def siguiente
 #     @kanji = Kanji.find_by(romaji: 'yon')
 #     http://stackoverflow.com/questions/2752231/random-record-in-activerecord
     offset = rand(Kanji.count)
     @kanji = Kanji.offset(offset).first
+    
+    render :json => @kanji
   end
+  
 end
