320fc481e0828a0e41ae3c2041901d3e1c79a234
Liber Dovat <liber.dovat@gmail.com>
2016-04-29 14:46:06 -0300

    agregadas las imágenes de los otros niveles

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -3,20 +3,12 @@ class InicioController < ApplicationController
   def index
     offset = rand(Kanji.count)
     @kanji = Kanji.offset(offset).first
-#     @kanji = Kanji.find(25)
   end
   
   def siguiente
-#     @kanji = Kanji.find_by(romaji: 'yon')
-#     http://stackoverflow.com/questions/2752231/random-record-in-activerecord
-
-    # offset = rand(Kanji.count)
-    # @kanji = Kanji.offset(offset).first
-
     byLevel = Kanji.where("nivel >= ?", params[:nivel])
     offset = rand(byLevel.count)
     @kanji = byLevel.offset(offset).first
-    
     render :json => @kanji
   end
 
